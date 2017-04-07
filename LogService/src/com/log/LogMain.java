937c68702eae195ee64cb4e3d6838d1e6495d40a
spi12der <nda12374@gmail.com>
2017-04-08 00:05:32 +0530

    Update the log service with the new messaging jar

--- LogService/src/com/log/LogMain.java
+++ LogService/src/com/log/LogMain.java
@@ -2,19 +2,32 @@ package com.log;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.json.simple.JSONObject;
 
 import com.message.Message;
 
 public class LogMain 
 {
+	static Message messageObject;
 	static Logger logger = Logger.getLogger(LogMain.class);
     
 	public static void main(String[] args)
     {
     	DOMConfigurator.configure("log4j-config.xml");
-        Message mObj=new Message();
-        mObj.recieveMessage();
+        messageObject=new Message(args[0],args[1],args[2],args[3]);
+        messageObject.recieveMessage();
+        processRequest();
     }
+	
+	public static void processRequest()
+	{
+		while(true)
+		{
+			JSONObject response=Message.messageQueue.poll();
+			if(response!=null)
+				LogMain.logMessage((String)response.get("logType"), (String)response.get("message"));
+		}	
+	}
 	
 	public static void logMessage(String msgType,String message)
 	{
