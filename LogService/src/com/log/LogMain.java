c06fa98a967bfc00d7924cfeedad460d1a679772
spi12der <nda12374@gmail.com>
2017-04-12 12:36:14 +0530

    Added code sending logs to gateway

--- LogService/src/com/log/LogMain.java
+++ LogService/src/com/log/LogMain.java
@@ -1,7 +1,11 @@
 package com.log;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.message.Message;
@@ -19,13 +23,25 @@ public class LogMain
         processRequest();
     }
 	
+	@SuppressWarnings("unchecked")
 	public static void processRequest()
 	{
 		while(true)
 		{
 			JSONObject response=Message.messageQueue.poll();
 			if(response!=null)
-				LogMain.logMessage((String)response.get("logType"), (String)response.get("message"));
+			{
+				if(response.containsKey("logType"))
+					LogMain.logMessage((String)response.get("logType"), (String)response.get("message"));
+				else
+				{
+					JSONObject message=new JSONObject();
+					message.put("queue", "gateway");
+					message.put("status", 1);
+					message.put("details", new LogMain().getLogs());
+					messageObject.sendMessage(message);
+				}	
+			}	
 		}	
 	}
 	
@@ -41,7 +57,41 @@ public class LogMain
 							break;				
 			case "FATAL":	logger.fatal(message);
 							break;				
-				
 		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public JSONObject getLogObject(String line)
+	{
+		JSONObject logObject=new JSONObject();
+		logObject.put("debug", line.split(" ")[2]);
+		int index=line.indexOf("-")+1;
+		line=line.substring(index);
+		index=line.indexOf(":");
+		String moduleName=line.substring(0, index).trim();
+		logObject.put("module", moduleName);
+		String log=line.substring(index+1).trim();
+		logObject.put("log", log);
+		return logObject;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public JSONArray getLogs()
+	{
+		JSONArray arr=new JSONArray();
+		try (BufferedReader br = new BufferedReader(new FileReader("platform.log"))) 
+		{
+		    String line;
+		    while ((line = br.readLine()) != null) 
+		    {
+		    	arr.add(getLogObject(line));
+		    }
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		System.out.println(arr);
+		return arr;
 	}
 }
