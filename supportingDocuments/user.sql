3b45b26d50bc826f9f339605a3ae2df3e9389527
ram2012k <rmaheta2012@gmail.com>
2017-04-15 22:36:34 +0530

    Update user.sql

--- supportingDocuments/user.sql
+++ supportingDocuments/user.sql
@@ -1,7 +1,7 @@
-create DATABASE IF NOT EXISTS project;
+create DATABASE IF NOT EXISTS admin;
 USE  project;
 DROP TABLE IF EXISTS user;
-create table user(
+create table sd_details(
 user_name char(50) PRIMARY KEY NOT NULL,
 password char(50) NOT NULL,
 user_type char(50) NOT NULL,
