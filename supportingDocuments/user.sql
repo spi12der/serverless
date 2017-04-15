783caaf1f6b90324c52de61c12ee508bcc9cb5c5
ram2012k <rmaheta2012@gmail.com>
2017-04-15 22:40:34 +0530

    Update user.sql

--- supportingDocuments/user.sql
+++ supportingDocuments/user.sql
@@ -1,5 +1,5 @@
 create DATABASE IF NOT EXISTS admin;
-USE  project;
+USE  admin;
 DROP TABLE IF EXISTS user;
 create table sd_details(
 user_name char(50) PRIMARY KEY NOT NULL,
@@ -7,3 +7,9 @@ password char(50) NOT NULL,
 user_type char(50) NOT NULL,
 email_id char(50)NOT NULL UNIQUE
 );
+DROP TABLE IF EXISTS service_info;
+create table service_info(
+  service char(50) PRIMARY KEY NOT NULL,
+  path char(50) NOT NULL,
+  type char(50) NOT NULL
+  );
