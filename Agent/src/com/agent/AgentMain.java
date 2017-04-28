4caefc222c71b5c20aa0f9dbf06778533f87c0b4
suryansh94 <agnihotrisuryansh55@gmail.com>
2017-04-28 23:32:46 +0530

    Updated Agent

--- Agent/src/com/agent/AgentMain.java
+++ Agent/src/com/agent/AgentMain.java
@@ -14,91 +14,104 @@ import org.json.simple.JSONObject;
 
 import com.message.Message;
 
-public class AgentMain {
+public class AgentMain 
+{
 	static Message messageObject;
-
-	public static void main(String[] args) {
-		messageObject = new Message(args[0], args[1], args[2], args[3]);
-		try {
+	
+	public static void main(String[] args) 
+	{
+		messageObject=new Message(args[0], args[1], args[2], args[3]);
+		try 
+		{
 			new AgentMain().updateStatus();
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
-
+	
 	/**
 	 * Method to send updated status to load balancer
-	 * 
 	 * @throws Exception
 	 */
 	@SuppressWarnings({ "unchecked", "static-access" })
-	public void updateStatus() throws Exception {
-		while (true) {
-			JSONObject response = getCPULoad();
+	public void updateStatus() throws Exception
+	{
+		while(true)
+		{
+			JSONObject response=getCPULoad();
 			response.put("type", "update_cpu");
 			response.put("queue", "loadbalancer");
 			messageObject.sendMessage(response);
 			Thread.currentThread().sleep(10000);
 		}
 	}
-
+	
 	/**
 	 * Method to get cpu load of current server
-	 * 
 	 * @return
-	 * @throws SocketException
+	 * @throws SocketException 
 	 */
 	@SuppressWarnings("unchecked")
-	public JSONObject getCPULoad() throws SocketException {
-		JSONObject response = new JSONObject();
+	public JSONObject getCPULoad() throws SocketException
+	{
+		JSONObject response=new JSONObject();
 		OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-		for (Method method : operatingSystemMXBean.getClass().getDeclaredMethods()) {
-			method.setAccessible(true);
-			if (method.getName().startsWith("get") && Modifier.isPublic(method.getModifiers())) {
-				Object value;
-				try {
-					value = method.invoke(operatingSystemMXBean);
-				} catch (Exception e) {
-					value = e;
-				}
-				String str = "";
-				str = method.getName() + " = " + value;
-				if (str.contains("getSystemCpuLoad")) {
-					response.put("cpu", str);
-					break;
-				}
-			}
+		for (Method method : operatingSystemMXBean.getClass().getDeclaredMethods()) 
+		{
+		    method.setAccessible(true);
+		    if (method.getName().startsWith("get") && Modifier.isPublic(method.getModifiers())) 
+		    {
+		        Object value;
+		        try 
+		        {
+		            value = method.invoke(operatingSystemMXBean);
+		        } 
+		        catch (Exception e) 
+		        {
+		            value = e;
+		        }
+                String str="";
+                str=method.getName() + " = " + value;
+                if(str.contains("getSystemCpuLoad"))
+                {
+                	response.put("cpu", str);
+                	break;
+                }       
+		    }
 		}
-		String ip = "";
-		@SuppressWarnings("rawtypes")
-		Enumeration e = NetworkInterface.getNetworkInterfaces();
-		int flag = 0;
-		while (e.hasMoreElements()) {
-			NetworkInterface n = (NetworkInterface) e.nextElement();
-			Enumeration ee = n.getInetAddresses();
-			while (ee.hasMoreElements()) {
-				InetAddress i = (InetAddress) ee.nextElement();
-				ip = i.getHostAddress();
-				if (flag == 1) {
-					// System.out.println(ip);
-					flag = 0;
-					break;
-				}
-				if (ip.contains("%")) {
-					int ind = ip.indexOf("%");
-					if (ip.charAt(ind + 1) == 'w') {
-						// System.out.println(i.getHostAddress());
-						flag = 1;
-					}
-
-				}
-			}
-		}
-		// InetAddress ipAddr = InetAddress.getLocalHost();
-		// response.put("ip", ipAddr.getHostAddress());
-		response.put("ip", ip);
-		// InetAddress ipAddr = InetAddress.getLocalHost();
-		// response.put("ip", ipAddr.getHostAddress());
+//            InetAddress ipAddr = InetAddress.getLocalHost();
+//            response.put("ip", ipAddr.getHostAddress());
+            @SuppressWarnings("rawtypes")
+    		Enumeration e = NetworkInterface.getNetworkInterfaces();
+    		int flag=0;
+    		while(e.hasMoreElements())
+    		{
+    		    NetworkInterface n = (NetworkInterface) e.nextElement();
+    		    Enumeration ee = n.getInetAddresses();
+    		    while (ee.hasMoreElements())
+    		    {
+    		        InetAddress i = (InetAddress) ee.nextElement();
+    		        String check = i.getHostAddress();
+    		        if(flag==1)
+    		        {
+//    		        	System.out.println("IP = "+check);
+    		        	response.put("ip", check);
+    		        	flag=0;
+    		        }
+    		        if(check.contains("%"))
+    		        {
+    		        	int ind = check.indexOf("%");
+    		        	if(check.charAt(ind+1)=='w')
+    		        	{
+//    				        System.out.println(i.getHostAddress());
+    				        flag=1;
+    		        	}
+    		        		
+    		        }
+    		    }
+    		}
 
 		return response;
 	}
